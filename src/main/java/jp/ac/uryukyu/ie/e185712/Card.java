eb913b35f6c98cc4226dfb61e800d1580688dd4a
e185712 <e185712@ie.u-ryukyu.ac.jp>
2019-01-21 13:18:38 +0900

    report6 トランプの生成

--- src/main/java/jp/ac/uryukyu/ie/e185712/Card.java
+++ src/main/java/jp/ac/uryukyu/ie/e185712/Card.java
@@ -1,5 +1,32 @@
 package jp.ac.uryukyu.ie.e185712;
 
-public class Card {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Card {
+    String ka ;
+    String[] suit = {"♡","♤","♢","♧"};
+    ArrayList<Integer> cards = new ArrayList<>();
+    ArrayList<Integer> player_hand = new ArrayList<>() ;
+
+    /**トランプを作る。♡:1~13,♤:14~26,♢:27~39,♧40~52とする
+     * cardsに(1~52)の数字を入れる。
+     * トランプをシャッフルする.
+     * トランプを配る。
+     * suitは,cardsのgetした値を13で割ることでマークを割り当てている。player_handに配ったカードを入れる。
+    */
+
+    public void make(){
+        for(int i = 1; i <= 52; i++){
+            cards.add(i-1,i-1);
+        }
+
+        Collections.shuffle(cards);
+
+        for (int l = 1;l <=2; l++){
+            ka = suit[cards.get(l-1) / 13];
+            player_hand.add(l-1,cards.get(l-1)%13+1) ;
+            System.out.println(ka + player_hand.get(l-1));
+        }
+    }
 }
